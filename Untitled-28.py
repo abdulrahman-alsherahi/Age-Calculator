77897633480d1c5103ed25e42f4ffcd3973203b8
Abdulrahman Al-Sharahi <alsherahi444@gmail.com>
2026-03-08 04:41:55 +0100

    Add files via upload

---
+++ Untitled-28.py
@@ -0,0 +1,26 @@
+
+
+# 1 - Age Calculator:
+
+
+
+# Age Calculator
+
+print("📅 --- Age Calculator ---")
+
+try:
+    age_years = int(input("Enter your age in years: "))
+except ValueError:
+    print("Please enter a valid number.")
+    exit()
+
+age_months = age_years * 12
+age_days = int(age_years * 365.25)
+age_hours = age_days * 24
+age_minutes = age_hours * 60
+
+print("\nYou have lived approximately:")
+print(age_months, "months")
+print(age_days, "days")
+print(age_hours, "hours")
+print(age_minutes, "minutes")
